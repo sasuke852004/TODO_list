5a9ccd4cffac9d0ae0a84292e38a299130f0fd22
sasuke852004 <itavbit2024@gmail.com>
2024-06-20 12:06:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import TodoList from "./components/TodoList";
+import TodoForm from "./components/TodoForm";
+import "./App.css";
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+    setTodos([...todos, { id: Date.now(), text, completed: false }]);
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div className="App">
+      <h1>TODO List</h1>
+      <TodoForm addTodo={addTodo} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TodoItem = ({ todo, toggleTodo, deleteTodo }) => {
+  return (
+    <div>
+      <input
+        type="checkbox"
+        checked={todo.completed}
+        onChange={() => toggleTodo(todo.id)}
+      />
+      <span>{todo.text}</span>
+      <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+    </div>
+  );
+};
+
+export default TodoItem;
